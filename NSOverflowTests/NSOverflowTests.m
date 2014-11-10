a18d4817ddb02fdc3ea6fcb0ac3663faffd15e49
Casey White <casey.r.white@gmail.com>
2014-11-10 12:23:43 -0800

    Initial commit

---
+++ NSOverflowTests/NSOverflowTests.m
@@ -0,0 +1,40 @@
+//
+//  NSOverflowTests.m
+//  NSOverflowTests
+//
+//  Created by Casey R White on 11/10/14.
+//  Copyright (c) 2014 casecode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface NSOverflowTests : XCTestCase
+
+@end
+
+@implementation NSOverflowTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
