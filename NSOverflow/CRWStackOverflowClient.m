16aa9789b825da47b02dcbd3927e893d0f139683
Casey White <casey.r.white@gmail.com>
2014-11-12 15:18:35 -0800

    Abstract json parsing for questions into seperate method

--- NSOverflow/CRWStackOverflowClient.m
+++ NSOverflow/CRWStackOverflowClient.m
@@ -16,7 +16,7 @@ static NSString * const API_KEY = @"eecC8QAViP40PkTd16RXrQ((";
 @interface CRWStackOverflowClient ()
 
 - (void)fetchObjectsAtURL:(NSString *)url completion:(void (^)(NSData *data, NSError *error))completion;
-
+- (NSMutableArray *)parseQuestionsFromJSON:(NSData *)questionData error:(NSError **)error;
 @end
 
 
@@ -40,34 +40,47 @@ static NSString * const API_KEY = @"eecC8QAViP40PkTd16RXrQ((";
     
     [self fetchObjectsAtURL:url completion:^(NSData *data, NSError *error) {
         NSError *fetchError = nil;
-        NSMutableArray *questions = nil;
+        NSArray *questions = nil;
         
         if (error) {
             fetchError = error;
         }
         else {
-            id objs = [NSJSONSerialization JSONObjectWithData:data options:0 error:&fetchError];
-            
-            if (!fetchError) {
-                questions = [NSMutableArray array];
-                
-                if ([objs isKindOfClass:[NSDictionary class]]) {
-                    NSArray *items = [(NSDictionary *)objs objectForKey:@"items"];
-                    
-                    [items enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-                        if ([obj isKindOfClass:[NSDictionary class]]) {
-                            CRWQuestion *question = [CRWQuestion questionFromDictionary:(NSDictionary *)obj];
-                            [questions addObject:question];
-                        }
-                    }];
-                }
-            }
+            questions = [self parseQuestionsFromJSON:data error:&fetchError];
+            NSLog(@"QUESTION COUNT: %lu", questions.count);
         }
         
+        if (fetchError) {
+            NSLog(@"UM ERROR");
+        }
         dispatch_async(dispatch_get_main_queue(), ^{
             completion(questions, fetchError);
         });
     }];
+}
+
+- (NSMutableArray *)parseQuestionsFromJSON:(NSData *)questionData error:(NSError *__autoreleasing *)error {
+    NSMutableArray *questions = nil;
+    id objs = [NSJSONSerialization JSONObjectWithData:questionData options:0 error:error];
+    
+    if (!*error) {
+        questions = [NSMutableArray array];
+
+        if ([objs isKindOfClass:[NSDictionary class]]) {
+            NSArray *items = [(NSDictionary *)objs objectForKey:@"items"];
+            
+            [items enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                if ([obj isKindOfClass:[NSDictionary class]]) {
+                    CRWQuestion *question = [CRWQuestion questionFromDictionary:(NSDictionary *)obj];
+                    [questions addObject:question];
+                }
+            }];
+        } else {
+            *error = [NSError errorWithDomain:NSOVERFLOW_DOMAIN code:-1 userInfo:nil];
+        }
+    }
+    
+    return questions;
 }
 
 - (void)fetchObjectsAtURL:(NSString *)url completion:(void (^)(NSData *, NSError *))completion {
@@ -96,6 +109,7 @@ static NSString * const API_KEY = @"eecC8QAViP40PkTd16RXrQ((";
                     fetchError = [NSError errorWithDomain:NSOVERFLOW_DOMAIN code:0 userInfo:userInfo];
                 }
                 else {
+                    NSLog(@"1");
                     fetchData = data;
                 }
             }
