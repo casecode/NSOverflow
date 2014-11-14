f7ace06afcbd99109bc9debe04511547745c2290
Casey White <casey.r.white@gmail.com>
2014-11-14 14:55:50 -0800

    Add custom bitmask to LoginVC for showing and hidden views depending on whether user is authenticated

--- NSOverflow/LoginVC.m
+++ NSOverflow/LoginVC.m
@@ -10,9 +10,28 @@
 #import "CRWStackOverflowClient.h"
 #import "OAuthVC.h"
 
+/*
+ *** NOTE:
+ The LoginVC uses a custom bitmask along with KVC and KVO for showing/hiding views.
+ The example here is silly and contrived, and the sole purpose was to learn/practice these concepts.
+*/
+
+typedef NS_OPTIONS(NSUInteger, LoginDisplayItem) {
+    LoginDisplayItemNone        = 0,
+    LoginMessageItem            = 1 << 0,
+    LoginButtonItem             = 1 << 1,
+    AuthenticatedMessageItem    = 1 << 2
+};
+
+static int kAuthenticationContext;
+
 @interface LoginVC ()
 
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UILabel *loginMessageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *authenticatedMessageLabel;
+
+@property (nonatomic, assign) LoginDisplayItem displayBitmask;
 
 @property (strong, nonatomic) CRWStackOverflowClient *apiClient;
 
@@ -20,10 +39,70 @@
 
 @implementation LoginVC
 
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"displayBitmask" context:&kAuthenticationContext];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.apiClient = [CRWStackOverflowClient sharedClient];
+    
+    [self addObserver:self forKeyPath:@"displayBitmask" options:NSKeyValueObservingOptionNew context:&kAuthenticationContext];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    if ([_apiClient isAuthenticated]) {
+        LoginDisplayItem items = AuthenticatedMessageItem;
+        [self setValue:[NSNumber numberWithInteger:items] forKey:@"displayBitmask"];
+    } else {
+        LoginDisplayItem items = (LoginMessageItem |
+                                  LoginButtonItem);
+        [self setValue:[NSNumber numberWithInteger:items] forKey:@"displayBitmask"];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (context == &kAuthenticationContext) {
+        if ([keyPath isEqualToString:@"displayBitmask"]) {
+            [self updateDisplayItems];
+        }
+        else {
+            NSLog(@"Unknown key being observed in Authentication Context");
+        }
+    }
+    else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+- (void)updateDisplayItems {
+
+    LoginDisplayItem loginMessageItem = LoginMessageItem;
+    if (loginMessageItem & _displayBitmask) {
+        self.loginMessageLabel.hidden = NO;
+    }
+    else {
+        self.loginMessageLabel.hidden = YES;
+    }
+    
+    LoginDisplayItem loginButtonItem = LoginButtonItem;
+    if (loginButtonItem & _displayBitmask) {
+        self.loginButton.hidden = NO;
+    }
+    else {
+        self.loginButton.hidden = YES;
+    }
+    
+    LoginDisplayItem authenticatedMessageItem = AuthenticatedMessageItem;
+    if (authenticatedMessageItem & _displayBitmask) {
+        self.authenticatedMessageLabel.hidden = NO;
+    }
+    else {
+        self.authenticatedMessageLabel.hidden = YES;
+    }
 }
 
 - (IBAction)loginPressed:(id)sender {
